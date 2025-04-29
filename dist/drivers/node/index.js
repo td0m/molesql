2fab2120d80305801d32b418561f711db0e8468a
Dominik Tarnowski <dominik@openvolt.com>
2025-04-29 14:07:39 +0100

    remove dist

--- dist/drivers/node/index.js
+++
@@ -1,3 +0,0 @@
-export * as t from "./types.js";
-export * from "./sql.js";
-export * from "./db.js";
--- dist/drivers/node/types.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ZodTypeDef } from "zod";
-import { z } from "zod";
-export declare const boolean: () => z.ZodEffects<z.ZodNumber, boolean, number>;
-export declare const json: <T, U = T>(t?: z.ZodType<T, ZodTypeDef, U>) => z.ZodEffects<z.ZodString, T, string>;
