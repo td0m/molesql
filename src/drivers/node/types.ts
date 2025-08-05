fcff3b7e9a4122e868d01ad556df1a0be4f1a995
Dominik Tarnowski <dominik@openvolt.com>
2025-08-05 21:44:04 +0100

    fix: zod v4 issues

--- src/drivers/node/types.ts
+++ src/drivers/node/types.ts
@@ -1,7 +1,7 @@
-import type { ZodTypeDef } from "zod";
+import type { ZodObject } from "zod";
 import { z } from "zod";
 
 export const boolean = () => z.number().transform((v) => v === 1);
 
-export const json = <T, U = T>(t: z.ZodType<T, ZodTypeDef, U> = z.any()) =>
+export const json = <T extends ZodObject>(t: T) =>
   z.string().transform((v) => t.parse(JSON.parse(v)));
